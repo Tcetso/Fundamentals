a1788891568d7d9029120628f5c63c59e1553f60
Tcetso <tcetso@yahoo.com>
2017-10-20 09:26:27 +0300

    fix list

---
+++ 15-17_Lists/10FlipList/Flip.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10FlipList
+    {
+    class Flip
+        {
+        static void Main(string[] args)
+            {
+            var nums = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            var even = true;
+            if (nums.Count%2 != 0)
+                {
+                even = false;
+                }
+
+            var t = nums.Count - 2;
+            for (int i = 1; i < (nums.Count/2); i++)
+                {
+                var temp = nums[i];
+                nums[i] = nums[t];
+                nums[t] = temp;
+                t--;
+                }
+            Console.WriteLine(string.Join(" ", nums));
+            }
+        }
+    }
---
+++ 15-17_Lists/EqualSum/EqualSum.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EqualSum
+{
+    class EqualSum
+    {
+        static void Main(string[] args)
+        {
+            var nums1 = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            var nums2 = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            for (int i = 0; i < nums1.Count; i++)
+            {
+                for (int j = 0; j < nums2.Count; j++)
+                {
+                    if (nums2[j] == nums1[i])
+                    {
+                        nums2.RemoveAt(j);
+                    }
+                }
+            }
+            var sum1 = 0;
+            var sum2 = 0;
+            SumLists(nums1, nums2, ref sum1, ref sum2);
+
+            if (sum1==sum2)
+            {
+                Console.WriteLine($"Yes. Sum: {sum1}");
+            }
+            else
+            {
+                Console.WriteLine("No. Diff: {0}", Math.Abs(sum1-sum2));
+            }
+            
+        }
+
+        private static void SumLists(List<int> nums1, List<int> nums2, ref int sum1, ref int sum2)
+        {
+            foreach (var number in nums1)
+            {
+                sum1 += number;
+            }
+            foreach (var number in nums2)
+            {
+                sum2 += number;
+            }
+        }
+    }
+}
