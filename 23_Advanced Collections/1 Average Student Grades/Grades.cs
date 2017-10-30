3c3d18c377f467d56b4281466d37fa6d96f957ee
Tcetso <tcetso@yahoo.com>
2017-10-30 13:21:43 +0200

    nested dictionaries

---
+++ 23_Advanced Collections/1 Average Student Grades/Grades.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Average_Student_Grades
+	{
+	class Grades
+		{
+		static void Main(string[] args)
+			{
+			var num = int.Parse(Console.ReadLine());
+			var studentGrades = new Dictionary<string, List<double>>();
+
+			for (int i = 0; i < num; i++)
+				{
+				var current = Console.ReadLine().Split(' ').ToList();
+				var name = current[0];
+				var grade = double.Parse(current[1]);
+				if (!studentGrades.ContainsKey(name))
+					{
+					studentGrades[name] = new List<double>();
+					}
+				studentGrades[name].Add(grade);
+				}
+
+			foreach (var student in studentGrades)
+				{
+				Console.Write("{0} -> ", student.Key);
+				for (int i = 0; i < student.Value.Count; i++)
+					{
+					Console.Write("{0:f2} ",student.Value[i]);
+					}
+				Console.WriteLine("(avg: {0:f2})", student.Value.Average());
+				}
+			}
+		}
+	}
---
+++ 23_Advanced Collections/2 Cities by Continent and Country/Cities.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Cities_by_Continent_and_Country
+	{
+	class Cities
+		{
+		static void Main(string[] args)
+			{
+			var num = int.Parse(Console.ReadLine());
+			var citiesData = new Dictionary<string, Dictionary<string, List<string>>>();
+
+			for (int i = 0; i < num; i++)
+				{
+				var input = Console.ReadLine().Split(' ').ToList();
+				var continent = input[0];
+				var country = input[1];
+				var city = input[2];
+				if (!citiesData.ContainsKey(continent))
+					{
+					citiesData[continent] = new Dictionary<string, List<string>>();
+					}
+				if (!citiesData[continent].ContainsKey(country))
+					{
+					citiesData[continent][country] = new List<string>();
+					}
+				citiesData[continent][country].Add(city);
+				}
+
+			var continentsList = citiesData.Keys;
+			foreach (var continent in continentsList)
+				{
+				Console.WriteLine($"{continent}:");
+
+				var countriesList = citiesData[continent].Keys;
+				foreach (var country in countriesList)
+					{
+					var citiesList = citiesData[continent][country];
+					Console.WriteLine(" {0} -> {1}", country, string.Join(", ", citiesList));
+					}
+				}
+
+			var item = new SortedSet<string>();
+			}
+		}
+	}
