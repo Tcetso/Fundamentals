c76d52653c630fbadd0661463262805d4cd904ab
Tcetso <tcetso@yahoo.com>
2017-11-06 17:28:57 +0200

    class

---
+++ 29_Classess/1 Exercises/Exers.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Exercises
+	{
+	class Exercises
+		{
+		public string Topic { get; set; }
+
+		public string CourseName { get; set; }
+
+		public string JudgeLink { get; set; }
+
+		public List<string> Problems { get; set; }
+		}
+
+	class Exers
+		{
+		static void Main(string[] args)
+			{
+			var input = Console.ReadLine();
+			var data = new List<Exercises>();
+
+			while (input != "go go go")
+				{
+				var courseInfo = input.Split(new[] { " -> ", ", " }, StringSplitOptions.RemoveEmptyEntries);
+				var newExerc = new Exercises();
+				newExerc.Topic = courseInfo[0];
+				newExerc.CourseName = courseInfo[1];
+				newExerc.JudgeLink = courseInfo[2];
+				newExerc.Problems = courseInfo.Skip(3).ToList();
+				data.Add(newExerc);
+				input = Console.ReadLine();
+				}
+			foreach (var exerc in data)
+				{
+				Console.WriteLine("Exercises: {0}", exerc.Topic);
+				Console.WriteLine("Problems for exercises and homework for the \"{0}\" course @ SoftUni.", exerc.CourseName);
+				Console.WriteLine("Check your solutions here: {0}", exerc.JudgeLink);
+				int count = 1;
+				foreach (var item in exerc.Problems)
+					{
+					Console.WriteLine("{0}. {1}", count, item);
+					count++;
+					}
+				}
+			}
+		}
+	}
---
+++ 29_Classess/2 Optimized Banking System/Banks.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Optimized_Banking_System
+	{
+	class BankAccounts
+		{
+		public string Bank { get; set; }
+		public string AccountName { get; set; }
+		public decimal Balance { get; set; }
+		}
+	class Banks
+		{
+		static void Main(string[] args)
+			{
+			var input = Console.ReadLine();
+			var accounts = new List<BankAccounts>();
+
+			while (input != "end")
+				{
+				var accParams = input.Split(new[] { " | " }, StringSplitOptions.RemoveEmptyEntries);
+				//var currAcc = new BankAccounts { Bank = accParams[0], AccountName = accParams[1], Balance= decimal.Parse(accParams[2]) };
+				accounts.Add(new BankAccounts { Bank = accParams[0], AccountName = accParams[1], Balance = decimal.Parse(accParams[2]) });
+				input = Console.ReadLine();
+				}
+
+			foreach (var item in accounts.OrderByDescending(x=>x.Balance).ThenBy(x=>x.Bank.Length))
+				{
+				Console.WriteLine($"{item.AccountName} -> {item.Balance} ({item.Bank})");
+				}
+			}
+		}
+	}
