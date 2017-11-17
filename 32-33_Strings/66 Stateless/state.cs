100e57a94e4c48cfbad5a8d23354fcbc13a22b69
Tcetso <tcetso@yahoo.com>
2017-11-17 17:36:18 +0200

    and more str

---
+++ 32-33_Strings/66 Stateless/state.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _66_Stateless
+	{
+	class state
+		{
+		static void Main(string[] args)
+			{
+			var state = Console.ReadLine();
+			while (state!= "collapse")
+				{
+				var fiction = Console.ReadLine();
+
+				while (fiction!=string.Empty)
+					{
+					var startindex = state.IndexOf(fiction);
+					while (startindex!=-1)
+						{
+						state = state.Remove(startindex, fiction.Length).Trim();
+						startindex = state.IndexOf(fiction);
+						}
+					if (fiction.Length>1)
+						{
+						fiction = fiction.Substring(1, fiction.Length - 2);
+						}
+					else
+						{
+						fiction = string.Empty;
+						}
+					}
+				if (state == string.Empty)
+					{
+					Console.WriteLine("(void)");
+					}
+				else
+					{
+					Console.WriteLine(state);
+					}
+				state = Console.ReadLine();
+				}
+
+			}
+		}
+	}
---
+++ 32-33_Strings/67 Pyramidic/pyramid.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _67_Pyramidic
+	{
+	class pyramid
+		{
+		static void Main(string[] args)
+			{
+			var num = int.Parse(Console.ReadLine());
+			var inputData = new List<string>();
+			var letter = new char();
+			var maxCount = 0;
+			for (int i = 0; i < num; i++)
+				{
+				inputData.Add(Console.ReadLine());
+				}
+			for (int i = 0; i < inputData.Count; i++)
+				{
+				for (int k = 0; k < inputData[i].Length; k++)
+					{
+					var currentLetter = inputData[i][k];
+					var cols = 1;
+					var count = 0;
+					for (int z = i+1; z < inputData.Count; z++)
+						{
+						var checkString = new string(currentLetter, cols + 2);
+						if (inputData[z].Contains(checkString))
+							{
+							cols += 2;
+							count++;
+							}
+						else
+							{
+							break;
+							}
+						}
+					if (count>maxCount)
+						{
+						maxCount = count;
+						letter = currentLetter;
+						}
+					}
+				}
+			var t = 1;
+			for (int i = 0; i < maxCount+1; i++)
+				{
+				Console.WriteLine(new string(letter, i+t));
+				t++;
+				}
+			}
+		}
+	}
